70691bae0e490ac8b6b068691caaec69c789f16e
moratojoan <moratojoan@gmail.com>
2023-11-13 19:11:44 +0100

    refactor: component Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Header from '@/components/Header';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,10 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header>
-          <h1>Lodgify Interview - Joan</h1>
-          <h2>FE Technical Challenge</h2>
-        </header>
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Lodgify Interview - Joan</h1>
+      <h2>FE Technical Challenge</h2>
+    </header>
+  );
+}
